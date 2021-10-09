7e7d47f3b0b6a155cd2b73f0a8a9f2ef70fcf372
Diego Romero <diegoromeropersonal@gmail.com>
2021-10-08 23:02:04 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <View style={styles.container}>
+          <View
+            style={{ backgroundColor: "#2d3", padding: 20, borderRadius: 10 }}
+          >
+            <Text>How are you!</Text>
+          </View>
+          <Text>Do that thing!</Text>
+        </View>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2df",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/Plans.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const Plans = () => {
+  return <View backgroundColor="#f42" />;
+};
+
+export default Plans;
